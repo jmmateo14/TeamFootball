4aed64e63b81d49193b1e9f7ee8704cec31cbe2c
jmmateo14 <jmmateo96@gmail.com>
2019-06-09 17:05:40 +0200

    Nuevos endpoints para crear, eliminar y actualizar un equipo

--- api_server/controllers/team.js
+++ api_server/controllers/team.js
@@ -8,7 +8,7 @@ var sendJSONresponse = function(res, status, content) {
     res.json(content);
   };
 
-
+//Encuentra el primer equipo
 module.exports.teamFindOne = function(req, res) {
     console.log('Finding team details', req.params);
     Team 
@@ -29,6 +29,7 @@ module.exports.teamFindOne = function(req, res) {
       });
     };
 
+    //Busca equipo por Id
     module.exports.teamFindById = function(req, res) {
         if (req.params && req.params.id) { 
             Team
@@ -56,6 +57,7 @@ module.exports.teamFindOne = function(req, res) {
         }
     };    
 
+    //Busca equipos por pais
     module.exports.teamFindByCountry = function(req, res) {
         if (req.params && req.params.country) { 
             Team
@@ -80,5 +82,108 @@ module.exports.teamFindOne = function(req, res) {
             return res
             .status(404)
             .send({"message": "No `country` in request"});
+        }
+    };
+
+    //Crea un equipo
+    //Revisar el array de jugadores.
+    module.exports.teamCreate = function(req, res) {
+        Team
+        .create({ 
+            name: req.body.name, 
+            country: req.body.country,
+            foundation: req.body.foundation,
+            coach: req.body.coach,
+            stadium: req.body.stadium,
+            stars_player: req.body.stars_player,
+            image_url: req.body.image_url
+        },function(err, team) {
+            if (err) { 
+                return res
+                .status(400)
+                .send(err);
+            }
+            return res 
+            .status(201)
+            .send(team);
+        });
+    };
+
+    //Elimina un equipo
+    module.exports.teamDelete = function(req, res) {
+        if (req.params && req.params.id) { 
+            Team
+            .findByIdAndDelete(req.params.id) 
+            .exec(
+                function(err, team) {
+                    if (err) { 
+                        return res
+                        .status(400)
+                        .send(err);
+                    }
+                    return res 
+                    .status(204)
+                    .send(null);
+                }
+            );
+        } else {
+            return res
+            .status(404)
+            .send({"message": "No id in the request"});
+        }
+    };
+
+    //Actualiza un equipo.
+    module.exports.teamUpdate = function(req, res) {
+        if (req.params && req.params.id) { 
+            Team
+            .findById(req.params.id) 
+            .exec(
+                function(err, team) {
+                    if (!team) { 
+                        return res
+                        .status(404)
+                        .send({"message": "no team found"});
+                    } else {
+                        if (req.body.name) { 
+                            team.name = req.body.name;
+                        }
+                        if (req.body.country) {
+                            team.country = req.body.country;
+                        }
+                        if (req.body.foundation) {
+                            team.foundation = req.body.foundation;
+                        }
+                        if (req.body.coach) {
+                            team.coach = req.body.coach;
+                        }
+                        if (req.body.stadium) {
+                            team.stadium = req.body.stadium;
+                        }
+                        if (req.body.stars_player) {
+                            team.stars_player = req.body.stars_player;
+                        }
+                        if (req.body.image_url) {
+                            team.image_url = req.body.image_url;
+                        }
+                        team.save(function (err, team) { 
+                            if (err) { 
+                                return res
+                                .status(404)
+                                .send(err);
+                            }
+                            else {
+                                return res 
+                                .status(200)
+                                .send(team);
+                            }
+                        });
+                    }
+                }
+            );
+        } else {
+            return res
+            .status(404)
+            .send({"message": "No id in the request"});
         }
     };
--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -5,5 +5,8 @@ var ctrlTeam = require('../controllers/team');
 router.get('/', ctrlTeam.teamFindOne); 
 router.get('/id/:id', ctrlTeam.teamFindById);
 router.get('/country/:country', ctrlTeam.teamFindByCountry);
+router.post('/team', ctrlTeam.teamCreate);
+router.delete('/team/:id', ctrlTeam.teamDelete);
+router.put('/team/:id', ctrlTeam.teamUpdate);
 
 module.exports = router;
