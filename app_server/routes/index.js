a8e70de1b7b32c05880306042dcee7f5c1e054c6
jmmateo14 <jmmateo96@gmail.com>
2019-06-15 13:29:12 +0200

    Añade nuevas rutas en el archivo routers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,10 @@ var ctrlTeams = require('../controllers/teams');
 
 /* GET home page. */
 router.get('/', ctrlTeams.homelist);
+router.get('/team/:teamId', ctrlTeams.team);
 router.get('/list', ctrlTeams.teamList); 
+router.get('/new', ctrlTeams.addTeam);
+router.post('/team', ctrlTeams.doAddTeam);
+router.post('/team/:teamId', ctrlTeams.doDeleteTeam);
 
 module.exports = router;
